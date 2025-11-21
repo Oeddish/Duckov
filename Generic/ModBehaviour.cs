a4b870121c2108b66c1fe9b748f936dd36524039
Oed <oedwak@comcast.net>
2025-11-21 11:30:06 -0800

    Initial commit of Duckov mods project - Includes Generic mod template based on DisplayItemValue example - DisplayItemValue example is copied from https://github.com/xvrsl/duckov_modding/ - Build copies output to Steam local and mods directories

---
+++ Generic/ModBehaviour.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Duckov.UI;
+using Duckov.Utilities;
+using ItemStatsSystem;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+// TODO: Rename when cloning template
+namespace Generic
+{
+
+    public class ModBehaviour : Duckov.Modding.ModBehaviour
+    {
+        TextMeshProUGUI _text = null;
+        TextMeshProUGUI Text
+        {
+            get
+            {
+                if (_text == null)
+                {
+                    _text = Instantiate(GameplayDataSettings.UIStyle.TemplateTextUGUI);
+                }
+                return _text;
+            }
+        }
+        void Awake()
+        {
+            /*
+            macOS:
+                ~/Library/Logs/TeamSoda/Duckov/Player.log       # Current session log
+                ~/Library/Logs/TeamSoda/Duckov/Player-prev.log  # Previous session log (auto-backup)
+            Windows:
+                C:\Users\<YourUsername>\AppData\LocalLow\TeamSoda\Duckov\Player.log
+                C:\Users\<YourUsername>\AppData\LocalLow\TeamSoda\Duckov\Player-prev.log
+            */
+            Debug.Log("Generic Loaded!!!");
+        }
+        void OnDestroy()
+        {
+            if (_text != null)
+                Destroy(_text);
+        }
+        void OnEnable()
+        {
+            ItemHoveringUI.onSetupItem += OnSetupItemHoveringUI;
+            ItemHoveringUI.onSetupMeta += OnSetupMeta;
+        }
+        void OnDisable()
+        {
+            ItemHoveringUI.onSetupItem -= OnSetupItemHoveringUI;
+            ItemHoveringUI.onSetupMeta -= OnSetupMeta;
+        }
+
+        private void OnSetupMeta(ItemHoveringUI uI, ItemMetaData data)
+        {
+            Text.gameObject.SetActive(false);
+        }
+
+        private void OnSetupItemHoveringUI(ItemHoveringUI uiInstance, Item item)
+        {
+            if (item == null)
+            {
+                Text.gameObject.SetActive(false);
+                return;
+            }
+            
+            Text.gameObject.SetActive(true);
+            Text.transform.SetParent(uiInstance.LayoutParent);
+            Text.transform.localScale = Vector3.one;
+            //Text.text = $"${item.GetTotalRawValue() / 2}";
+            Text.text = $"Generic: ${item.CanBeSold}";
+            Text.fontSize = 20f;
+        }
+    }
+}
